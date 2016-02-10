4f859b75b049f741e4cf4f156762ae6529708374
Martin Pribyl <ununik@gmail.com>
2016-02-10 15:13:20 +0100

    add map to footer

--- views/footer/footer.php
+++ views/footer/footer.php
@@ -9,5 +9,6 @@ $foot = "<a href='$authorWeb' title='$author' target='_blank'><img src='images/p
 $foot .= "<a href='mailto:horp@seznam.cz' title='mail - horp@seznam.cz' target='_blank'><img src='images/page/mail.png' alt='horp@seznam.cz' title='mail - horp@seznam.cz'></a>";
 $foot .= "<a href='index.php?page=eshop' title='eshop' target='_blank'><img src='images/page/eshop.png' alt='eshop' title='eshop'></a>";
 $foot .= "<a href='https://www.facebook.com/horpcz/' title='facebook' target='_blank'><img src='images/page/fb.png' alt='facebook' title='facebook'></a>";
+$foot .= "<a href='http://mapy.cz/zakladni?x=14.3135142&y=50.7286513&z=17&source=addr&id=9154508' title='kontakt na mapě' target='_blank'><img src='images/page/map.png' alt='kontakt na mapě' title='kontakt na mapě'></a>";
 
 return $foot;
