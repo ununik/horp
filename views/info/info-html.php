2cf7fa0a706e2de3c25bf4aa53e5fd99e570b0a4
Martin Pribyl <ununik@gmail.com>
2016-04-08 16:57:45 +0200

    last changes

--- views/info/info-html.php
+++ views/info/info-html.php
@@ -3,41 +3,23 @@ $container = "<h1>Časté dotazy</h1>";
 $container .= "<ol>";
 
 $container .= "<li><h2>Jaké chyty jsou vhodné pro děti?</h2>
-                <p>Samozřejmě záleží na věku dítěte, ale obecně je vhodné hledat ve velikosti M a L a Dětské. Co se týče tvaru, pak pozitivní , tzn. „ chytovaté“</p></li>";
+                <p>Samozřejmě záleží na věku dítěte, ale obecně je vhodné hledat v kategoriích M a L a Dětské. Co se týče tvaru, pak tzv. pozitivní (chytovaté).</p></li>";
 
-$container .= "<li><h2>Epoxidové, nebo polyesterové?</h2>
-                <p>Každý typ má své výhody i nevýhody, epoxidové mají vysokou pevnost, mají menší tření při úchopu chytu – šetří pokožku prstů. Polyesterové mají jasnější barvy a vyšší tření.</p></li>";
-
-$container .= "<li><h2>Jak je to s barvami chytů?</h2>
-                <p>Jakýkoli chyt lze vyrobit v jakékoli barvě. Barvy chytů na naší webové stránce je pouze orientační (např. velikost XL nevyrábíme pouze v modré, jak je na stránkách, ale ve všech barvách). V případě objednávky zasíláme mix barev (nejrychlejší varianta). Pokud si přejete konkrétní barvy chytů, je třeba to uvést v objednávce (výroba zabere cca
-                    1 – 2 týdny podle velikosti objednávky). Na přání je možné vyrobit i dvoubarevné chyty, přirážka k základní ceně je však 10 %. Možnosti výběru barev viz <a href='index.php?page=vzornik' target='_blank'>Vzorník</a>.</p></li>";
-
-$container .= "<li><h2>Jak poznám, jak je daný chyt velký?</h2>
-                <p>U každého chytu je jeho rozměr v centimetrech. V případě koulí se jedná o průměry.</p></li>";
-
-$container .= "<li><h2>Dodáváte k chytům i šrouby?</h2>
-                <p>Ano dodáváme, ale ne automaticky, je třeba je k chytům přiobjednat.</p></li>";
+$container .= "<li><h2>Epoxidové nebo polyesterové chyty?</h2>
+                <p>Každý typ má své výhody a nevýhody, epoxidové mají vysokou pevnost a menší tření při úchopu  chytu – šetří pokožku prstů. Polyesterové mají jasnější barvy a vyšší tření.</p></li>";
 
 $container .= "<li><h2>Je možné domluvit slevu?</h2>
-                <p>Ano, sleva se však odvíjí od množství odebraných chytů.</p></li>";
-
-$container .= "<li><h2>Kolik platím za dopravu?</h2>
-                <p>Zasíláme poštou, počítáme přesné poštovné, které zavisí na váze balíku. Při platbě předem zasíláme fa. k proplacení, kde je vyčíslená i položka poštovného. Při platbě
-na dobírku je možné na požádání zaslat výslednou sumu včetně poštovného. Výpočet poštovného se může drobně lišit, ale spoléháme na vaši vstřícnost.</p></li>";
-
-$container .= "<li><h2>Jak rozmístit chyty na stěně?</h2>
-                <p>Chyty rozmístit na plochu tak, aby jednotlivé vzdálenosti mezi chyty byly přibližně stejné. Do spodní části stěny použít chyty oblejších tvarů a naopak čím výš tím větší chyt.</p></li>";
+                <p>Ano, sleva se však odvíjí od množství odebraných chytů – viz. Ceník.</p></li>";
 
 $container .= "<li><h2>Jak chyty připevnit?</h2>
-                <p>Nejběžnější a nejvhodnější způsob ukotvení chytů je na překližkovou desku o tl. min.
-18 mm pomocí narážecích matic M10, naražených do předem vyvrtaného otvoru 11,5 mm a imbusového šroubu M10. Samozřejmě je možno uchycovat i do jiných povrchů jako např. betonový panel pomocí ocelových narážecích kotev 12 mm a imbusového šroubu M10, cihlového zdiva při použití plastových hmoždinek 10 mm a vrutů 8 mm nebo
-i do dřevěné trámové konstrukce vruty 8 mm. Vždy je ale potřeba, aby povrch, kam chyty upevňujeme, byl rovný. Na nerovné povrchy jsou z důvodu jejich větší pevnosti vhodnější EPOXY chyty. Utahovat chyty je zapotřebí s citem a toto platí zejména
-u menších velikostí.</p></li>";
+                <p>Nejběžnější a nejvhodnější způsob ukotvení chytů je na překližkovou desku o tl. min. 18 mm pomocí narážecích matic M10. Samozřejmě je možno uchycovat i do jiných povrchů jako např. betonový panel pomocí ocelových narážecích kotev 12 mm a imbusového šroubu M10, cihlového zdiva při použití mosazných narážecích kotev 12mm a imbusového šroubu M10  nebo i do dřevěné trámové konstrukce  vruty 8 mm. Vždy je třeba, aby povrch, kam chyty upevňujeme, byl rovný. Na nerovné povrchy jsou z důvodu jejich větší pevnosti vhodnější EPOXY chyty. Utahovat chyty je zapotřebí s citem, a toto platí zejména u menších velikostí.</p></li>";
 
 $container .= "<li><h2>Kolik chytů počítat na 1m<sup>2</sup>?</h2>
                 <p>Nejčastěji se počítá s 5 – 8ks/1m<sup>2</sup>,ale je nutno zohlednit, kdo na chytech poleze.
 Tak např. pro dětské využití je potřeba počítat s plným počtem, ale pro delší lezeckou linii, na které polezou trénovanější lezci, postačí i 3 ks/1m<sup>2</sup>.</p></li>";
 
+$container .= "<li><h2>Jak rozmístit chyty na stěně?</h2>
+                <p>Chyty rozmístit na plochu tak, aby jednotlivé vzdálenosti mezi chyty byly přibližně stejné. Do spodní části stěny použít chyty oblejších tvarů a naopak čím výš tím větší chyt. </p></li>";
 
 $container .= "</ol>";
 
